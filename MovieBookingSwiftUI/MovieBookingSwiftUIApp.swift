04452fd0648e04f097feebdc55695260b64bfcd5
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-08 13:46:41 -0600

    Initial Commit

---
+++ MovieBookingSwiftUI/MovieBookingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingSwiftUIApp.swift
+//  MovieBookingSwiftUI
+//
+//  Created by Brenda Saavedra  on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

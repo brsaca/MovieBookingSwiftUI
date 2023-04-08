be2f4c2463f4f003b67cd79eb9acac5ca48226ef
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-08 14:14:42 -0600

    fix: templates addition

---
+++ TicketView.swift
@@ -0,0 +1,20 @@
+//
+//  TicketView.swift
+//  
+//
+//  Created by Brenda Saavedra  on 08/04/23.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicketView()
+    }
+}

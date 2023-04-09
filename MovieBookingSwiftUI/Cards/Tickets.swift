83c261a454e72fad488e802c4a9edba9d9abc624
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-08 19:32:30 -0600

    style: add bottom of ticket

---
+++ MovieBookingSwiftUI/Cards/Tickets.swift
@@ -0,0 +1,20 @@
+//
+//  Tickets.swift
+//  MovieBookingSwiftUI
+//
+//  Created by Brenda Saavedra  on 08/04/23.
+//
+
+import SwiftUI
+
+struct Tickets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tickets_Previews: PreviewProvider {
+    static var previews: some View {
+        Tickets()
+    }
+}

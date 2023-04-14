c7296f9c88a12c06b982d1424f4efe3be699f512
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-14 16:39:35 -0600

    feat: wrap vStack in NavigationView

--- MovieBookingSwiftUI/ContentView.swift
+++ MovieBookingSwiftUI/ContentView.swift
@@ -15,22 +15,24 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            TabView(selection: $currentTab) {
-                HomeView().tag(Tab.home)
-                Text("Location")
-                    .tag(Tab.location)
-                TicketView()
-                    .tag(Tab.ticket)
-                Text("Category")
-                    .tag(Tab.category)
-                Text("Profile")
-                    .tag(Tab.profile)
+        NavigationView {
+            VStack(spacing: 0) {
+                TabView(selection: $currentTab) {
+                    HomeView().tag(Tab.home)
+                    Text("Location")
+                        .tag(Tab.location)
+                    TicketView()
+                        .tag(Tab.ticket)
+                    Text("Category")
+                        .tag(Tab.category)
+                    Text("Profile")
+                        .tag(Tab.profile)
+                }
+                
+                CustomTabBar(currentTab: $currentTab)
             }
-            
-            CustomTabBar(currentTab: $currentTab)
+            .ignoresSafeArea(.keyboard)
         }
-        .ignoresSafeArea(.keyboard)
     }
 }
 
